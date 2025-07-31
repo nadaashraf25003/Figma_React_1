4d1b653dc97d69e9585049582a3c254fae7474d6
Nada Ashraf <nadanadaashraf25@gmail.com>
2025-07-31 03:56:55 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import "./Header.css";
+import reactjsicon from '../assets/reactjs-icon.svg'
 export function Header() {
   return (
     <>
       <header>
         <div className="logo">
-          <img src="/src/assets/reactjs-icon.svg" />
+          <img src={reactjsicon} />
         </div>
         <div className="title">
           <p>ReactFacts</p>
